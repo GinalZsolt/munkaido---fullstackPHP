3dd4024d16b19544dd4d8fbc6d13ea7216c21a64
ginalzsolt <ginalzsolt@turr.hu>
2023-03-09 12:45:01 +0100

    loader update2

--- loader.php
+++ loader.php
@@ -6,15 +6,15 @@
         case "workers":{include("dolgozok.php");break;}
 
         case "workers_del":{include("delworker.php");break;}
-        case "shift_del":{include("delshift.php");break;}
+        case "shifts_del":{include("delshift.php");break;}
 
-        case "shift_add":{include("newshift.php");break;}
+        case "shifts_add":{include("newshift.php");break;}
         case "workers_add":{include("newworker.php");break;}
 
         case "workers_info":{include("infoworker.php");break;}
-        case "shift_info":{include("infoshift.php");break;}
+        case "shifts_info":{include("infoshift.php");break;}
 
-        case "shift_mod":{include("modshift.php");break;}
+        case "shifts_mod":{include("modshift.php");break;}
         case "workers_mod":{include("modworker.php");break;}
 
     }
